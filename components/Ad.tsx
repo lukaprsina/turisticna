c224f0b9ee23ef23dc65bc40f1cd8fe63a4a0c5c
Luka <luk.prsina@gmail.com>
2022-06-10 10:03:22 +0000

    oglasi kmalu v kinu zraven tebe

--- components/Ad.tsx
+++ components/Ad.tsx
@@ -5,11 +5,20 @@ function Ad() {
     return <Box sx={{
         margin: "50px 0",
     }}>
-        <img
+        {/* <img
             width="500px"
             src="https://the-slovenia.com/wp-content/uploads/2020/04/F013496-aquafun_naravna_zdravilisca_terme_catez_ciril_jazbec_17-photo-s.jpg"
-            alt="oglas" /><br />
+            alt="oglas"
+        />
+        <br />
         <Typography variant="caption" component="p" textAlign="center">Pridite v Toplice DANES!</Typography>
+         */}
+        <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6736306320712601"
+            crossOrigin="anonymous">                
+        </script>
+        
     </Box>
 }
 
